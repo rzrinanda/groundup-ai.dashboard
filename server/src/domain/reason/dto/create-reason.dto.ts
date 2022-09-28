3970fb377718273a1d00e60c5118c2d65dc48394
Rizal Zulfikar Rinanda <rzrinanda@gmail.com>
2022-09-28 16:07:35 +0700

    reasons

--- server/src/domain/reason/dto/create-reason.dto.ts
+++ server/src/domain/reason/dto/create-reason.dto.ts
@@ -1,1 +1,11 @@
-export class CreateReasonDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateReasonDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly machine : string
+
+    @IsString()
+    @IsNotEmpty()
+    readonly reason : string
+}
--- server/src/domain/reason/entities/reason.entity.ts
+++ server/src/domain/reason/entities/reason.entity.ts
@@ -1,1 +1,12 @@
-export class Reason {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Reason {
+    @Prop()
+    machine : string
+    
+    @Prop()
+    reason : string
+}
+
+export const ReasonSchema = SchemaFactory.createForClass(Reason);
